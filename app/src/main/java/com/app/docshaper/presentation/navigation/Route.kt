fe4fd3c5f3edd9b48cb95b94213220c2f709f17e
navneet <navneetpalgithub@gmail.com>
2024-08-03 19:15:30 +0530

    fixed the navigation

---
+++ app/src/main/java/com/app/docshaper/presentation/navigation/Route.kt
@@ -0,0 +1,17 @@
+package com.app.docshaper.presentation.navigation
+
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+object OnBoarding
+
+@Serializable
+object Home
+
+@Serializable
+object Files
+
+@Serializable
+object Search
+
