c38bd32cffd3b209a9539214cb2fa9864102a8d1
navneet <navneetpalgithub@gmail.com>
2024-08-19 11:11:00 +0530

    Updated the OnBoardingScreen

--- app/src/main/java/com/app/docshaper/presentation/onboarding_screen/components/OnBoardingBottom.kt
+++ app/src/main/java/com/app/docshaper/presentation/onboarding_screen/components/OnBoardingBottom.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
@@ -16,7 +18,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.app.docshaper.ui.theme.urbanist_bold
+import com.app.docshaper.ui.theme.urbanist_medium
 
 
 @Composable
@@ -28,8 +35,9 @@ fun OnBoardingBottom() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.height(25.dp))
         OnBoardingBottomButton()
-        Spacer(modifier = Modifier.height(15.dp))
+        Spacer(modifier = Modifier.height(25.dp))
     }
 
 }
@@ -46,24 +54,43 @@ private fun OnBoardingBottomButton() {
             onClick = {
 
             },
-
+            modifier = Modifier
+                .padding(horizontal = 10.dp)
+                .weight(1f),
             colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.onPrimaryContainer),
-            shape = RoundedCornerShape(15.dp),
+            shape = CircleShape
         ) {
             Text(
                 text = "Skip",
-                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier
+                    .padding(vertical = 10.dp),
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Normal,
+                    fontFamily = urbanist_bold,
+                ),
+                color = MaterialTheme.colorScheme.onPrimary,
             )
         }
         Button(
             onClick = {
 
             },
+            modifier = Modifier
+                .padding(10.dp)
+                .weight(1f),
             colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.onPrimary),
-            shape = RoundedCornerShape(15.dp),
+            shape = CircleShape,
         ) {
             Text(
                 text = "Next",
+                modifier = Modifier
+                    .padding(vertical = 10.dp),
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Normal,
+                    fontFamily = urbanist_bold,
+                ),
                 color = MaterialTheme.colorScheme.secondary
             )
         }
--- app/src/main/java/com/app/docshaper/ui/theme/Type.kt
+++ app/src/main/java/com/app/docshaper/ui/theme/Type.kt
@@ -9,29 +9,18 @@ import androidx.compose.ui.unit.sp
 import com.app.docshaper.R
 
 
-val urbanist_medium = FontFamily(Font(R.font.urbanist_medium))
 val urbanist_regular = FontFamily(Font(R.font.urbanist_regular))
-
+val urbanist_medium = FontFamily(Font(R.font.urbanist_medium))
+val urbanist_bold = FontFamily(Font(R.font.urbanist_bold))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    headlineLarge = TextStyle(
-        fontFamily = urbanist_medium,
-        fontWeight = FontWeight.Medium,
-        fontSize = 60.sp,
-        lineHeight = 43.sp
-    ),
-    headlineMedium = TextStyle(
-        fontFamily = urbanist_medium,
-        fontWeight = FontWeight.Medium,
-        fontSize = 42.sp,
-        lineHeight = 30.sp
-    ),
-    headlineSmall = TextStyle(
-        fontFamily = urbanist_medium,
-        fontWeight = FontWeight.Medium,
-        fontSize = 32.sp,
-        lineHeight = 23.sp
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
     ),
 
     /* Other default text styles to override
